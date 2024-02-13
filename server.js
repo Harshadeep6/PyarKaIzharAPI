b1e18c0df54d3dc3a93c4a6b37a5fa95c293f53e
Harshadeep6 <harshadeepmore@gmail.com>
2024-02-14 00:01:29 +0530

    ITIW

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   // Other CORS headers if needed
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
